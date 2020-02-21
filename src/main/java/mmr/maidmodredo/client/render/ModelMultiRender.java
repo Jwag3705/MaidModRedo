8cc281d5b40a285e730016ae922c2a170b30d174
pentantan <shutux500@gmail.com>
2020-02-21 14:56:45 +0900

    spawnegg texture changed amd fixed handrender

--- src/main/java/mmr/maidmodredo/client/render/ModelMultiRender.java
+++ src/main/java/mmr/maidmodredo/client/render/ModelMultiRender.java
@@ -7,16 +7,21 @@ import mmr.maidmodredo.client.maidmodel.ModelBaseSolo;
 import mmr.maidmodredo.entity.LittleMaidBaseEntity;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.Matrix4f;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.Pose;
+import net.minecraft.entity.item.HangingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Direction;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.LightType;
 
 import java.util.List;
 
@@ -224,6 +229,53 @@ public class ModelMultiRender<T extends LittleMaidBaseEntity> extends MobRendere
             renderModelMulti(entityIn, entityYaw, partialTicks, matrixStackIn, bufferIn, packedLightIn, 2);
             matrixStackIn.pop();
         }
+
+        Entity entity = entityIn.getLeashHolder();
+        if (entity != null) {
+            this.renderLeash(entityIn, partialTicks, matrixStackIn, bufferIn, entity);
+        }
+    }
+
+    private <E extends Entity> void renderLeash(T entityLivingIn, float partialTicks, MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, E leashHolder) {
+        matrixStackIn.push();
+        double d0 = (double) (MathHelper.lerp(partialTicks * 0.5F, leashHolder.rotationYaw, leashHolder.prevRotationYaw) * ((float) Math.PI / 180F));
+        double d1 = (double) (MathHelper.lerp(partialTicks * 0.5F, leashHolder.rotationPitch, leashHolder.prevRotationPitch) * ((float) Math.PI / 180F));
+        double d2 = Math.cos(d0);
+        double d3 = Math.sin(d0);
+        double d4 = Math.sin(d1);
+        if (leashHolder instanceof HangingEntity) {
+            d2 = 0.0D;
+            d3 = 0.0D;
+            d4 = -1.0D;
+        }
+
+        double d5 = Math.cos(d1);
+        double d6 = MathHelper.lerp((double) partialTicks, leashHolder.prevPosX, leashHolder.getPosX()) - d2 * 0.7D - d3 * 0.5D * d5;
+        double d7 = MathHelper.lerp((double) partialTicks, leashHolder.prevPosY + (double) leashHolder.getEyeHeight() * 0.7D, leashHolder.getPosY() + (double) leashHolder.getEyeHeight() * 0.7D) - d4 * 0.5D - 0.25D;
+        double d8 = MathHelper.lerp((double) partialTicks, leashHolder.prevPosZ, leashHolder.getPosZ()) - d3 * 0.7D + d2 * 0.5D * d5;
+        double d9 = (double) (MathHelper.lerp(partialTicks, entityLivingIn.renderYawOffset, entityLivingIn.prevRenderYawOffset) * ((float) Math.PI / 180F)) + (Math.PI / 2D);
+        d2 = Math.cos(d9) * (double) entityLivingIn.getWidth() * 0.4D;
+        d3 = Math.sin(d9) * (double) entityLivingIn.getWidth() * 0.4D;
+        double d10 = MathHelper.lerp((double) partialTicks, entityLivingIn.prevPosX, entityLivingIn.getPosX()) + d2;
+        double d11 = MathHelper.lerp((double) partialTicks, entityLivingIn.prevPosY, entityLivingIn.getPosY());
+        double d12 = MathHelper.lerp((double) partialTicks, entityLivingIn.prevPosZ, entityLivingIn.getPosZ()) + d3;
+        matrixStackIn.translate(d2, -(1.6D - (double) entityLivingIn.getHeight()) * 0.5D, d3);
+        float f = (float) (d6 - d10);
+        float f1 = (float) (d7 - d11);
+        float f2 = (float) (d8 - d12);
+        float f3 = 0.025F;
+        IVertexBuilder ivertexbuilder = bufferIn.getBuffer(RenderType.leash());
+        Matrix4f matrix4f = matrixStackIn.getLast().getPositionMatrix();
+        float f4 = MathHelper.fastInvSqrt(f * f + f2 * f2) * 0.025F / 2.0F;
+        float f5 = f2 * f4;
+        float f6 = f * f4;
+        int i = this.getBlockLight(entityLivingIn, partialTicks);
+        int j = this.getBlockLight(entityLivingIn, partialTicks);
+        int k = entityLivingIn.world.getLightFor(LightType.SKY, new BlockPos(entityLivingIn.getEyePosition(partialTicks)));
+        int l = entityLivingIn.world.getLightFor(LightType.SKY, new BlockPos(leashHolder.getEyePosition(partialTicks)));
+        renderSide(ivertexbuilder, matrix4f, f, f1, f2, i, j, k, l, 0.025F, 0.025F, f5, f6);
+        renderSide(ivertexbuilder, matrix4f, f, f1, f2, i, j, k, l, 0.025F, 0.0F, f5, f6);
+        matrixStackIn.pop();
     }
 
     // TODO いらん？
